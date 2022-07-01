a86a51b508aa821dee8c12e40d2f70002ec56964
psd9988 <psd9988@gmail.com>
2022-07-01 19:33:05 +0530

    5th question added

--- Js pattern questions Assignement14/q5.js
+++ Js pattern questions Assignement14/q5.js
@@ -2,8 +2,21 @@ let n = 5;
 
 for (let i = 1; i <= 5; i++) {
 let string  = '';
+let space = ' ';
 for(let j = 1; j<=5; j++){
-  string = string + '*';
-}
+  if (i+j==4 || j == (n+i)-3){
+    string = string + i
+  }if(string != '3'){if(j == (n+ i) - 7){
+    string = string + (6-i)
+  }}
+  if(i+j==8 && i==4){
+    string = string + (2)
+  }
+
+
+ 
+ 
+  string = string + space;
+}console.log(string);
 
 }
